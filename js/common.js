f643e05c68ee96023fd0ed34004d48d23743659b
etna-scorpio <etna.scorpio@gmail.com>
2015-11-04 18:49:52 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -15,4 +15,28 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
+	$('.js-popup').click(function(event) {
+		event.preventDefault();
+		$('.' + $(this).data('popup')).fadeIn();
+		$('body').addClass('is-overflow');
+	});
+
+	$('.js-close').click(function(e) {
+		e.preventDefault();
+		$(this).parents('.popup').fadeOut();
+		$('body').removeClass('is-overflow');
+	});
+
+	// // zoom
+	// $('.js-zoom').elevateZoom({
+	// 	zoomWindowFadeIn: 400,
+	// 	zoomWindowFadeOut: 400,
+	// 	zoomWindowWidth: 300,
+	// 	zoomWindowHeight: 300,
+	// 	zoomWindowOffetx: 10,
+	// 	borderSize: 1,
+	// 	lensSize: 200,
+	// 	zoomWindowPosition: 14
+	// });
+
 });
